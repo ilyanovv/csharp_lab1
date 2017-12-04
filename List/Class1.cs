d1cb9e19b55ef4122dd8357419fd92851eeacba2
ilyanovv <ilyanovv@gmail.com>
2017-12-04 20:48:53 +0300

    init commit

---
+++ List/Class1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyListImpl
+{
+    public class MyList<T> : IList<T>
+    {
+        public class Node
+        {
+            public T value;
+            public Node next;
+
+            public Node(T value, Node next)
+            {
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+
+        private int _size;
+        private Node _first;
+        private Node _last;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > _size - 1 || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                Node cur_node = _first;
+                for(int i = 0; i < index; i++)
+                {
+                    cur_node = cur_node.next;
+                }
+                return cur_node.value;
+            }
+
+            set
+            {
+                if (index > _size - 1 || index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                Node cur_node = _first;
+                for (int i = 0; i < index; i++)
+                {
+                    cur_node = cur_node.next;
+                }
+                cur_node.value = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _size;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_size == 0)
+            {
+                _first = _last = new Node(item, null);
+            }
+            else
+            {
+                Node prev = _last;
+                prev.next = new Node(item, null);
+                _last = prev.next;
+            }
+            _size++;
+        }
+
+        public void Clear()
+        {
+            _size = 0;
+            _first = null;
+            _last = null;
+        }
+
+        public bool Contains(T item)
+        {
+            Node cur_node = _first;
+            for (int i = 0; i < _size; i++)
+            {
+                if (cur_node.value.Equals(item))
+                {
+                    return true;
+                }
+                else
+                {
+                    cur_node = cur_node.next;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                array[arrayIndex + i] = this[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            Node cur_node = _first;
+            for (int i = 0; i < _size; i++)
+            {
+                if (cur_node.value.Equals(item))
+                {
+                    return i; 
+                }
+                else
+                {
+                    cur_node = cur_node.next;
+                }  
+            }
+            return -1;
+        }
+
+  
+        public void Insert(int index, T item)
+        {
+            if (index > _size - 1 || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Node new_node = new Node(item, null);
+            if (index == 0)
+            {
+                new_node.next = _first;
+                _first = new_node;
+            }
+            else
+            {
+                Node prev = _first;
+                Node next = _first.next;
+                int i = 1; 
+                while (i != index)
+                {
+                    prev = prev.next;
+                    next = next.next;
+                    i++;
+                }
+                prev.next = new_node;
+                new_node.next = next;
+            }
+            _size++;
+        }
+
+        public bool Remove(T item)
+        {
+            Node cur_node = _first;
+            Node prev_node = null;
+            for (int i = 0; i < _size; i++)
+            {
+                if (cur_node.value.Equals(item))
+                {
+                    if (i == 0)
+                    {
+                        _first = cur_node.next;
+                    }
+                    else
+                    {
+                        prev_node.next = cur_node.next;
+                        if (i == _size - 1)
+                        {
+                            _last = prev_node;
+                        }
+                    }
+
+                    _size--;
+                    return true;
+                }
+                prev_node = cur_node;
+                cur_node = cur_node.next;
+            }
+            return false;
+        }
+
+
+        public void RemoveAt(int index)
+        {
+            if (index > _size - 1 || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+
+            if (index == 0)
+            {
+                _first = _first.next;
+            }
+
+            else
+            {
+                Node prev_node = _first;
+                Node cur_node = _first.next;
+                for (int i = 1; i < index; i++)
+                {
+                    prev_node = cur_node;
+                    cur_node = cur_node.next;
+                }
+
+                prev_node.next = cur_node.next;
+                if(index == _size - 1)
+                {
+                    _last = prev_node;
+                }
+            }
+            _size--;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+        public override string ToString()
+        {
+            if (_size == 0)
+                return "";
+            String str = "";
+            for (int i = 0; i < _size - 1; i++)
+            {
+                str += this[i].ToString() + ", ";
+            }
+            str += this[_size - 1].ToString();
+            return str;
+        }
+
+        public static MyList<T> fromArray(T[] arr)
+        {
+            MyList<T> list = new MyList<T>();
+            foreach (T elem in arr)
+            {
+                list.Add(elem);
+            }
+            return list;
+        }
+    }
+}
---
+++ ListUnitTest/UnitTest1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyListImpl;
+
+namespace ListUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnElementsOfListCorrectly()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(6, list[5]);
+            Assert.AreEqual(4, list[3]);
+        }
+
+        [TestMethod]
+        public void ShouldSetElementsOfListCorrectly()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            list[0] = 10;
+            list[5] = 60;
+            list[3] = 40;
+            Assert.AreEqual(10, list[0]);
+            Assert.AreEqual(60, list[5]);
+            Assert.AreEqual(40, list[3]);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCountOfElementsCorrectly()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(6, list.Count);
+            list = new MyList<int>();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void ShouldAddNewElementsCorrectly()
+        {
+            IList<int>list = new MyList<int>();
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+            Assert.AreEqual("0, 1, 2", list.ToString());
+        }
+
+        [TestMethod]
+        public void ShouldClearListCorrectly()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            list.Clear();
+            Assert.AreEqual("", list.ToString());
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void ShouldCheckWhetherListContainsSpecifiedElement()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            Assert.AreEqual(true, list.Contains(1));
+            Assert.AreEqual(true, list.Contains(3));
+            Assert.AreEqual(true, list.Contains(6));
+            Assert.AreEqual(false, list.Contains(100));
+        }
+
+        [TestMethod]
+        public void ShouldCopyToArrayCorrectly()
+        {
+            IList<int> list = MyList<int>.fromArray(new int[] { 1, 2, 3, 4, 5, 6 });
+            int[] array = new int[6];
+            list.CopyTo(array, 0);
+            Assert.AreEqual("1 2 3 4 5 6", String.Join(" ", array));
+            array = new int[10];
+            list.CopyTo(array, 3);
+            Assert.AreEqual("0 0 0 1 2 3 4 5 6 0", String.Join(" ", array));
+        }
+
+        [TestMethod]
+        public void ShouldReturnIndexOfElement()
+        {
+            IList<String> list = MyList<String>.fromArray(
+                new String[] {"a", "b", "c", "d", "a", "b"});
+            Assert.AreEqual(0, list.IndexOf("a"));
+            Assert.AreEqual(1, list.IndexOf("b"));
+            Assert.AreEqual(3, list.IndexOf("d"));
+            Assert.AreEqual(-1, list.IndexOf("z"));
+        }
+
+        [TestMethod]
+        public void ShouldInsertElementCorrectly()
+        {
+            IList<String> list = MyList<String>.fromArray(
+                new String[] { "a", "b", "c", "d"});
+            list.Insert(0, "z");
+            Assert.AreEqual("z, a, b, c, d", list.ToString());
+
+            list.Insert(list.Count - 1, "w");
+            Assert.AreEqual("z, a, b, c, w, d", list.ToString());
+
+            list.Insert(2, "y");
+            Assert.AreEqual("z, a, y, b, c, w, d", list.ToString());
+        }
+
+        [TestMethod]
+        public void ShouldRemoveByElement()
+        {
+            IList<String> list = MyList<String>.fromArray(
+                new String[] { "a", "b", "c", "d", "a" });
+            Assert.AreEqual(true, list.Remove("a"));
+            Assert.AreEqual("b, c, d, a", list.ToString());
+
+            Assert.AreEqual(true, list.Remove("a"));
+            Assert.AreEqual("b, c, d", list.ToString());
+
+            Assert.AreEqual(true, list.Remove("c"));
+            Assert.AreEqual("b, d", list.ToString());
+
+            Assert.AreEqual(false, list.Remove("a"));
+        }
+
+        [TestMethod]
+        public void ShouldRemoveByIndex()
+        {
+            IList<String> list = MyList<String>.fromArray(
+                new String[] { "a", "b", "c", "d", "a" });
+            list.RemoveAt(0);
+            Assert.AreEqual("b, c, d, a", list.ToString());
+
+            list.RemoveAt(list.Count-1);
+            Assert.AreEqual("b, c, d", list.ToString());
+
+            list.RemoveAt(1);
+            Assert.AreEqual("b, d", list.ToString());
+        }
+
+        [TestMethod]
+        public void ShouldHandleWithIteratorCorrectly()
+        {
+            IList<String> list = MyList<String>.fromArray(
+                new String[] { "a", "b", "c", "d", "a" });
+            IEnumerator<String> iterator = list.GetEnumerator();
+            StringBuilder stringBuilder = new StringBuilder();
+            while (iterator.MoveNext())
+            {
+                stringBuilder.Append(iterator.Current).Append(", "); 
+            }
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            Assert.AreEqual(list.ToString(), stringBuilder.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ShouldThrowExceptionWhenIncorrectIndex()
+        {
+            IList<String> list = MyList<String>.fromArray(
+              new String[] { "a", "b", "c", "d", "a" });
+            String s = list[5];
+        }
+    }
+}
